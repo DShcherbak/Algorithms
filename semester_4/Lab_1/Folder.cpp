4b0fc569dfd35935dc97259d5d325e4a36b98595
DShcherbak <denys.shcherbak.official@gmail.com>
2020-03-24 20:47:10 +0200

    Removed useless files

---
+++ semester_4/Lab_1/Folder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sadoffnick on 24.03.20.
+//
+
+#include "Folder.h"
---
+++ semester_4/Lab_1/Folder.h
@@ -0,0 +1,14 @@
+//
+// Created by sadoffnick on 24.03.20.
+//
+
+#ifndef LAB_1_FOLDER_H
+#define LAB_1_FOLDER_H
+
+
+class Folder {
+
+};
+
+
+#endif //LAB_1_FOLDER_H
